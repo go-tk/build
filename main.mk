6a19db1f1ad8a62fd4aea4002651eb253a7c4107
Roy O'Young <roy2220@outlook.com>
2021-01-20 22:21:50 +0800

    Add support for Docker

--- main.mk
+++ main.mk
@@ -5,7 +5,24 @@ override .DEFAULT_GOAL := help
 override makefile := $(lastword $(MAKEFILE_LIST))
 override build_dir := $(patsubst %/,%,$(dir $(makefile)))
 
-##targets:
+ifdef USE_DOCKER ###################################################################################
+override targets := $(or $(MAKECMDGOALS),$(.DEFAULT_GOAL))
+
+.PHONY: $(targets)
+.ONESHELL:
+$(targets):
+	export BUILD_DIR=$(build_dir)/docker
+	@cp go.{mod,sum} $${BUILD_DIR}
+	export COMPOSE_FILE=$${BUILD_DIR}/docker-compose.yml$${COMPOSE_FILE:+$${COMPOSE_PATH_SEPARATOR:-:}$${COMPOSE_FILE}}
+	export COMPOSE_PROJECT_NAME=$${COMPOSE_PROJECT_NAME:-$(notdir $(CURDIR))}
+	trap "docker-compose down --rmi=local --remove-orphans" EXIT
+	docker-compose build
+	docker-compose run --rm build make $(MFLAGS) USE_DOCKER= _BUILD_DIR=$${BUILD_DIR} --makefile=$(makefile) $@
+
+else ###############################################################################################
+_BUILD_DIR := $(build_dir)
+
+## targets:
 
 .PHONY: all
 ##  all: generate & fmt & lint & vet & test
@@ -24,22 +41,22 @@ generate:
 ##  fmt:
 ##    Format source code with command 'goimports'.
 ##    Custom command-line options could be provided via variable 'GOIMPORTS_FLAGS'.
-fmt: | $(build_dir)/bin/goimports
-	@go fmt -n ./... | grep --perl-regexp --only-matching '(?<=^| )[^ ]+\.go(?=$$| )' | xargs $| -format-only -l -w $(GOIMPORTS_FLAGS)
+fmt: | $(_BUILD_DIR)/bin/goimports
+	@go fmt -n ./... | grep -o '[^ ]\+.go$$' | xargs $| -format-only -l -w $(GOIMPORTS_FLAGS)
 
-$(build_dir)/bin/goimports: | $(build_dir)/go.mod
-	@cd $(build_dir); go build -o bin/goimports golang.org/x/tools/cmd/goimports
+$(_BUILD_DIR)/bin/goimports: | $(_BUILD_DIR)/bin/go.mod
+	@cd $(@D); go build -o $(@F) golang.org/x/tools/cmd/goimports
 
 .PHONY: lint
 ##
 ##  lint:
 ##    Check the coding style with command 'golint'.
 ##    Custom command-line options could be provided via variable 'GOLINT_FLAGS'.
-lint: | $(build_dir)/bin/golint
+lint: | $(_BUILD_DIR)/bin/golint
 	@$| $(filter-out -set_exit_status,$(GOLINT_FLAGS)) ./... | $(build_dir)/scripts/golint-filter.bash
 
-$(build_dir)/bin/golint: | $(build_dir)/go.mod
-	@cd $(build_dir); go build -o bin/golint golang.org/x/lint/golint
+$(_BUILD_DIR)/bin/golint: | $(_BUILD_DIR)/bin/go.mod
+	@cd $(@D); go build -o $(@F) golang.org/x/lint/golint
 
 .PHONY: vet
 ##
@@ -73,9 +90,10 @@ help: override .SHELLFLAGS := $(subst -o xtrace,,$(.SHELLFLAGS))
 help:
 	@sed -n 's/^##//p' $(makefile)
 
-$(build_dir)/go.mod:
-	@cd $(build_dir); go mod init build
+$(_BUILD_DIR)/bin/go.mod:
+	@mkdir --parents $(@D); cd $(@D); go mod init bin
 
 ##
 ##example:
 ##  make all GO_TEST_FLAGS='-race -cover'
+endif ##############################################################################################
